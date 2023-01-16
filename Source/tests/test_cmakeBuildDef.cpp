376e150de342c04066a2a93a127619d607a41dbd
Ultranerdguy <noreply@localhost>
2023-01-16 17:09:14 +0000

    HDV-7 Added command line argument reader. Added tests

--- Source/tests/test_cmakeBuildDef.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "hdv_config.h"
-
-int main()
-{
-  std::cout << "HDV-" << HDV::VERSION::MAJOR << '.' << HDV::VERSION::MINOR << '.' << HDV::VERSION::PATCH << '\n';
-  return 0;
-}
